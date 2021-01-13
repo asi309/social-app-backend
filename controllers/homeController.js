824d6aac34451c04811814ff93c4dcac778e7a5a
Asidipta Chaudhuri <asidiptachaudhuri@gmail.com>
2021-01-13 10:52:16 +0530

    Added pagination

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -5,6 +5,8 @@ const Post = require('../models/Post');
 
 module.exports = {
   async showAllPosts(req, res) {
+    const currentPage = req.query.page || 1;
+    const perPage = 3;
     try {
       const following_docs = await Following.find({
         follower: req.user_id,
@@ -17,31 +19,45 @@ module.exports = {
       }
 
       let feedPosts = [];
+      let totalDocs;
       for (following_doc of following_docs) {
+        totalDocs = await Post.find({
+          author: following_doc.following._id,
+        }).countDocuments();
+
         const posts = await Post.find({
           author: following_doc.following._id,
-        });
-        for (post of posts) {
-          await post.populate('author', '-password').execPopulate();
-          feedPosts.push(post);
-        }
+        })
+          .populate('author', '-password')
+          .sort({ createdAt: -1 })
+          .skip((currentPage - 1) * perPage)
+          .limit(perPage);
+
+        feedPosts = feedPosts.concat(posts);
+        // for (post of posts) {
+        //   await post.populate('author', '-password').execPopulate();
+        //   feedPosts.push(post);
+        // }
       }
 
       if (feedPosts.length === 0) {
         return res.status(200).json({
           message: 'Nothing to show here',
           length: 0,
+          totalPosts: totalDocs,
         });
       }
 
-      feedPosts.sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt));
+      // feedPosts.sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt));
 
       return res.status(200).json({
         message: 'Found posts',
         feedPosts,
-        length: feedPosts.length,
+        length: perPage < totalDocs ? perPage : totalDocs,
+        totalPosts: totalDocs,
       });
     } catch (error) {
+      console.log(error);
       res.status(400).json({ message: 'Cannot load feed' });
     }
   },
--- controllers/postController.js
+++ controllers/postController.js
@@ -25,11 +25,28 @@ module.exports = {
     }
   },
   async getPostByUser(req, res) {
+    const { user_id } = req.headers;
+    const currentPage = req.query.page || 1;
+    const perPage = 3;
     try {
-      const posts = await Post.find({ author: req.user_id });
-      posts.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+      const totalDocs = await Post.find({
+        author: user_id,
+      }).countDocuments();
+      const posts = await Post.find({ author: user_id })
+        .populate('author', '-password')
+        .sort({ createdAt: -1 })
+        .skip((currentPage - 1) * perPage)
+        .limit(perPage);
 
-      return res.status(200).json(posts);
+      // posts.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
+      return res
+        .status(200)
+        .json({
+          posts,
+          length: perPage < totalDocs ? perPage : totalDocs,
+          totalPosts: totalDocs,
+        });
     } catch (error) {
       return res.status(400).send();
     }
